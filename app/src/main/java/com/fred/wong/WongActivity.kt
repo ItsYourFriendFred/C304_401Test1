4be7c876a4269f5d59f131134e0a7f4db72b2cad
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2024-06-28 01:53:32 -0400

    Initial commit

---
+++ app/src/main/java/com/fred/wong/WongActivity.kt
@@ -0,0 +1,24 @@
+// 301199984
+
+package com.fred.wong
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// Second Activity; since my name, Fred starts with a letter from A-N inclusive, use checkboxes
+
+class WongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_wong)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
