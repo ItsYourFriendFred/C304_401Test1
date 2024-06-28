8571df6a72d6bed322e7a2975dc592713b9b60f0
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2024-06-28 03:20:10 -0400

    Added main to 2nd screen intent logic + actionBar

--- app/src/main/java/com/fred/wong/FredActivity.kt
+++ app/src/main/java/com/fred/wong/FredActivity.kt
@@ -2,8 +2,14 @@
 
 package com.fred.wong
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -18,5 +24,29 @@ class FredActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Setting the title to Hands-On Test 1 COMP-304 401 instead of app name (FredWong_COMP304_001_Hands-On_Test1_S24)
+        // since app name title is too long and gets cut off
+        supportActionBar?.title = getString(R.string.actionBar_title_truncated)
+
+        // Retrieve both the ImageButton and Button controls
+        val imageButtonMain: ImageButton = findViewById(R.id.imageButtonMain)
+        val buttonMain: Button = findViewById(R.id.buttonMain)
+
+        // Set OnClickListeners for both button types
+        imageButtonMain.setOnClickListener {
+            val intent = Intent(this, WongActivity::class.java)
+            startActivity(intent)
+        }
+        buttonMain.setOnClickListener {
+            val intent = Intent(this, WongActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overall_menu, menu)
+        return true
     }
 }
--- app/src/main/java/com/fred/wong/WongActivity.kt
+++ app/src/main/java/com/fred/wong/WongActivity.kt
@@ -2,7 +2,11 @@
 
 package com.fred.wong
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +23,25 @@ class WongActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overall_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
